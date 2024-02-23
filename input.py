321162e439dbf171e2455fef434fb3bac0094ec6
jpv219 <j.valdes20@imperial.ac.uk>
2024-02-23 17:00:49 +0000

    Created Path config and overall class structure

--- input.py
+++ input.py
@@ -5,7 +5,53 @@
 ### Department of Chemical Engineering, Imperial College London
 ##########################################################################
 
-import numpy as np
 import pandas as pd
+import os
+import configparser
 
+class PathConfig:
 
+    def __init__(self):
+        self._config = configparser.ConfigParser()
+        self._config.read(os.path.join(os.getcwd(), 'config/config_paths.ini'))
+
+    @property
+    def fig_savepath(self):
+        return self._config['Path']['figs']
+
+    @property
+    def input_savepath(self):
+        return self._config['Path']['input']
+
+    @property
+    def raw_datapath(self):
+        return self._config['Path']['csv']
+    
+    @property
+    def label_datapath(self):
+        return self._config['Path']['doe']
+
+class DataReader(PathConfig):
+
+    def __init__(self, file_paths):
+        super().__init__()
+        self._file_paths = file_paths
+        
+    def combine_csv(self):
+        pass
+
+class DataProcessor(PathConfig):
+
+    def __init__(self):
+        super().__init__()
+
+class DataPackager(PathConfig):
+
+    def __init__(self):
+        super().__init__()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
