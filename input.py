73fb9a42dab6539b268154acbe06e2ac6c84fcc3
jpv219 <j.valdes20@imperial.ac.uk>
2024-02-23 11:45:22 +0000

    First commit on Active Learing Reg toolkit

---
+++ input.py
@@ -0,0 +1,11 @@
+##########################################################################
+#### Raw CSV data processing and PCA dimensionality reduction
+#### Author : Juan Pablo Valdes
+### First commit: Feb 2024
+### Department of Chemical Engineering, Imperial College London
+##########################################################################
+
+import numpy as np
+import pandas as pd
+
+
