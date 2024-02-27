5c18814c18c688ab8a62ef7932a295180b4e813f
jpv219 <j.valdes20@imperial.ac.uk>
2024-02-27 14:04:24 +0000

    First commit regression model script

---
+++ reg_train.py
@@ -0,0 +1,10 @@
+##########################################################################
+#### Regression model train and deployment
+#### Author : Juan Pablo Valdes
+### First commit: Feb 2024
+### Department of Chemical Engineering, Imperial College London
+##########################################################################
+
+import numpy as np
+import pandas as pd
+import pickle
